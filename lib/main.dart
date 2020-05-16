3bc32659072aa1a9ddfc50e023eacb9aea53675c
joe_fachrizal <joefachrizal@gmail.com>
2020-05-16 23:29:43 +0700

    Implement Material Design UI with Dart > add TextStyle colors and fontSize > add appBar to show title Apps

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "My Apps Flutter",
-    home: Material(
-      color: Colors.lightBlueAccent,
-      child: Center(
-        child: Text(
-          "Hello",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 40.0),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Test Flutter Apps"),
+      ),
+      body: Material(
+        color: Colors.lightBlueAccent,
+        child: Center(
+          child: Text(
+            "Hello",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 40.0),
+          ),
         ),
       ),
     ),
